491de43576dfaa6712557fc1fffe3f83b203fd2d
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2020-06-18 12:11:31 +0200

    implemented all existing item types

--- GildedRose.cc
+++ GildedRose.cc
@@ -14,79 +14,39 @@ void GildedRose::updateQuality()
             if (item.quality > 50) item.quality = 50;
             continue;
         }
+        if (item.name == "Sulfuras, Hand of Ragnaros") {
+            continue;
+        }
+        if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+            item.sellIn -= 1;
+            // struct Lambda {
+            //     Item& item; // capture
+
+            //     int operator() () {
+            //         if (item.sellIn < 0) return -item.quality;
+            //         if (item.sellIn < 5) return +3;
+            //         if (item.sellIn < 10) return +2;
+            //         return +1;
+            //     }
+            // };
+            // item.quality += Lambda{item}();
+            item.quality += [&] {
+                if (item.sellIn < 0) return -item.quality;
+                if (item.sellIn < 5) return +3;
+                if (item.sellIn < 10) return +2;
+                return +1;
+            }();
+            if (item.quality > 50) item.quality = 50;
+            continue;
+        }
+        item.sellIn -= 1;
+        item.quality -= item.sellIn < 0 ? 2 : 1;
+        if (item.quality < 0) item.quality = 0;
+        continue;
 
         // if (item.name == "Conjured Mana Cake") {
         //     // TODO
         //     continue;
         // }
-        if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-        {
-            if (items[i].quality > 0)
-            {
-                if (items[i].name != "Sulfuras, Hand of Ragnaros")
-                {
-                    items[i].quality = items[i].quality - 1;
-                }
-            }
-        }
-        else
-        {
-            if (items[i].quality < 50)
-            {
-                items[i].quality = items[i].quality + 1;
-
-                if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (items[i].sellIn < 11)
-                    {
-                        if (items[i].quality < 50)
-                        {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-
-                    if (items[i].sellIn < 6)
-                    {
-                        if (items[i].quality < 50)
-                        {
-                            items[i].quality = items[i].quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        if (items[i].name != "Sulfuras, Hand of Ragnaros")
-        {
-            items[i].sellIn = items[i].sellIn - 1;
-        }
-
-        if (items[i].sellIn < 0)
-        {
-            if (items[i].name != "Aged Brie")
-            {
-                if (items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (items[i].quality > 0)
-                    {
-                        if (items[i].name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            items[i].quality = items[i].quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    items[i].quality = items[i].quality - items[i].quality;
-                }
-            }
-            else
-            {
-                if (items[i].quality < 50)
-                {
-                    items[i].quality = items[i].quality + 1;
-                }
-            }
-        }
     }
 }
